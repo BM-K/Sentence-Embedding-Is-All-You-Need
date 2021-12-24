7389dd7bf694565cc3fef0fad076b379ad5a7c9a
bongminkim <klbm126@gmail.com>
2021-12-24 14:08:40 +0900

    initial commit

---
+++ get_model_checkpoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install gdown
+gdown --folder https://drive.google.com/drive/folders/https://drive.google.com/drive/folders/1orQxudCmdOLvRUFJdWEEK31l7IOVUnWs?usp=sharing -O Checkpoint
---
+++ get_model_dataset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install gdown
+gdown --folder https://drive.google.com/drive/folders/https://drive.google.com/drive/folders/140QpBbBPWXlqsbGZM1SpzhKDLMznq39B?usp=sharing -O Dataset
