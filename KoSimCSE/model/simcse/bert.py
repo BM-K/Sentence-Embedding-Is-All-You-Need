f3f2d9405254e51e2cd8656293f04f85ed367a21
BM-K <klbm126@khu.ac.kr>
2022-05-21 00:24:33 +0900

    Update bert.py

--- KoSimCSE/model/simcse/bert.py
+++ KoSimCSE/model/simcse/bert.py
@@ -11,41 +11,42 @@ class BERT(nn.Module):
 
         if mode == 'train':
             
-            _, anchor_pooler = self.bert(input_ids=inputs['anchor']['source'],
+            anchor_pooler, _ = self.bert(input_ids=inputs['anchor']['source'],
                                          token_type_ids=inputs['anchor']['token_type_ids'],
                                          attention_mask=inputs['anchor']['attention_mask'],
                                          return_dict=False)
-        
-            _, positive_pooler = self.bert(input_ids=inputs['positive']['source'],
+            
+            positive_pooler, _ = self.bert(input_ids=inputs['positive']['source'],
                                            token_type_ids=inputs['positive']['token_type_ids'],
                                            attention_mask=inputs['positive']['attention_mask'],
                                            return_dict=False)
 
-            _, negative_pooler = self.bert(input_ids=inputs['negative']['source'],
+            negative_pooler, _ = self.bert(input_ids=inputs['negative']['source'],
                                            token_type_ids=inputs['negative']['token_type_ids'],
                                            attention_mask=inputs['negative']['attention_mask'],
                                            return_dict=False)
-
-            return anchor_pooler, positive_pooler, negative_pooler
+            
+            return anchor_pooler[:, 0], positive_pooler[:, 0], negative_pooler[:, 0]
 
         else:
-            _, sentence_1_pooler = self.bert(input_ids=inputs['sentence_1']['source'],
+            sentence_1_pooler, _ = self.bert(input_ids=inputs['sentence_1']['source'],
                                              token_type_ids=inputs['sentence_1']['token_type_ids'],
                                              attention_mask=inputs['sentence_1']['attention_mask'],
                                              return_dict=False)
-
-            _, sentence_2_pooler = self.bert(input_ids=inputs['sentence_2']['source'],
+            
+            sentence_2_pooler, _ = self.bert(input_ids=inputs['sentence_2']['source'],
                                              token_type_ids=inputs['sentence_2']['token_type_ids'],
                                              attention_mask=inputs['sentence_2']['attention_mask'],
                                              return_dict=False)
+        
+            return sentence_1_pooler[:, 0], sentence_2_pooler[:, 0]
 
-            return sentence_1_pooler, sentence_2_pooler
 
     def encode(self, inputs, device):
     
-        _, embeddings = self.bert(input_ids=inputs['source'].to(device),
+        embeddings, _ = self.bert(input_ids=inputs['source'].to(device),
                                   token_type_ids=inputs['token_type_ids'].to(device),
                                   attention_mask=inputs['attention_mask'].to(device),
                                   return_dict=False)
 
-        return embeddings
+        return embeddings[:, 0]
